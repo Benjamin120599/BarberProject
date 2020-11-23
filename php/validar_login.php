f3eda51f2850e20314e7d7e1611d9c84cab49a80
Benjamin Viramontes <benja120599@gmail.com>
2020-11-22 20:13:45 -0600

    Se añadió el archivo que autentica los usuarios

---
+++ php/validar_login.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    include_once 'abrir_conexion.php';
+
+    $user = $_POST['caja_usuario'];
+    $pass = $_POST['caja_contraseña'];
+
+    $sentencia = $conexion->prepare("SELECT * FROM Clientes WHERE User_Client = ? AND Password_Client = ?;");
+
+    $sentencia->execute([$user, $pass]);
+    $datos = $sentencia->fetch(PDO::FETCH_ASSOC);
+    
+    if($datos) {
+        echo '<br>'.$datos['User_Client'];
+        echo '<br>'.$datos['Password_Client'];
+        echo '<br>'.$datos['Nombre_Client'];
+        echo '<br>'.$datos['Primer_Ap_Client'];
+        echo '<br>'.$datos['Segundo_Ap_Client'];
+        echo '<br>'.$datos['Telefono_Client']; 
+    } else {
+        echo '<br>No existe el usuario';
+    }
+    
+
+?>
