9cd64078195fcb2e1521e75a47cfea46a1bf8996
Benjamin Viramontes <benja120599@gmail.com>
2020-12-10 03:49:06 -0600

    Se cambió un titulo

--- vistas/Inicio/agregar_cita.php
+++ vistas/Inicio/agregar_cita.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['user'])) {
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>Inicio</title>
+    <title>Agendar Citas</title>
     <link href="css/styles.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
@@ -82,7 +82,7 @@ if (!isset($_SESSION['user'])) {
                             Modificar Cita
                         </a>
 
-                        <a class="nav-link" href="cancelar_cita.php">
+                        <a class="nav-link" href="eliminar_cita.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-calendar-times"></i></div>
                             Cancelar Cita
                         </a>
